fb8644dc85d882643e47486a105fd05cac020661
dianatoporkova <dianatoporkova>
2025-10-07 19:38:55 +0300

    initial commit

--- PZ_2/PZ_2_1.py
+++ PZ_2/PZ_2_1.py
@@ -1,1 +1,9 @@
-print ("Hello GitHub")
+try:
+    num = int (input ("Введите число большее 999: "))
+    if num <= 999:
+        print ("Ошибка: число должно быть больше 999")
+    else:
+        hund = (num // 100) % 10
+        print (hund)
+except ValueError:
+    print ("Ошибка. Попробуй ввести число.")
