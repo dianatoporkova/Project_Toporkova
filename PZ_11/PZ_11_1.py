6c10dd42bd5068473f3da154c86befd931bf2678
dianatoporkova <dianatoporkova>
2026-02-27 17:55:21 +0300

    initial commit

--- PZ_11/PZ_11_1.py
+++ PZ_11/PZ_11_1.py
@@ -3,17 +3,13 @@
 
 from functools import reduce
 marks = [2, 5, 5, 4, 3, 3]
-m1 = 0
-m2 = 0
-m3 = 0
-m4 = 0
-marks_2 = len ([m1 + 1 for i in marks if i == 2])
+marks_2 = len ([1 for i in marks if i == 2])
 print('Двоек:', marks_2)
-marks_3 = len ([m2 + 1 for i in marks if i == 3])
+marks_3 = len ([1 for i in marks if i == 3])
 print('Троек:', marks_3)
-marks_4 = len ([m3 + 1 for i in marks if i == 4])
+marks_4 = len ([1 for i in marks if i == 4])
 print('Четвёрок:', marks_4)
-marks_5 = len ([m4 + 1 for i in marks if i == 5])
+marks_5 = len ([1 for i in marks if i == 5])
 print('Пятёрок:', marks_5)
 n = reduce (lambda x, y: x + y, marks)
 print ('Итоговая оценка за месяц:', round (n / len (marks)))
