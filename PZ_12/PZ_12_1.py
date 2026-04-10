e7958dc9a62dff98cba7ab17a7c9a3574a15bd8b
dianatoporkova <dianatoporkova>
2026-04-10 15:18:23 +0300

    initial commit

--- PZ_12/PZ_12_1.py
+++ PZ_12/PZ_12_1.py
@@ -1,7 +1,8 @@
 #Вариант 22. В матрице найти минимальный элемент в предпоследнем столбце.
 
+import random
 n = int (input ('Введите размер матрицы: '))
-m = [[j * n + i for i in range(n)] for j in range(n)]
+m = [[random.randint(1, 100) for i in range(n)] for j in range(n)]
 print (m)
 z = min(map(lambda i: i[-2], m))
 print ('Минимальный элемент:', z)
